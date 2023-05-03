9ba929a55cabdbfdfa98602a96e03d4cadfb2020
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-05-02 22:47:55 -0300

    exe 067

---
+++ #067-tabuada-v3.py
@@ -0,0 +1,10 @@
+""" Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado 
+pelo usuário. O programa será interrompido quando o número solicitado for negativo."""
+n = 0
+while True:
+    n = int(input('Número para tabuada: '))
+    if n < 0:
+        break
+    for i in range(1,11):
+        print('{} x {:2} = {}'.format(n, i, n*i))
+print('Programa encerrado')
